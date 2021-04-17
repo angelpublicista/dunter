fa2b584f6d34034fabbb3d3e84809a379cc3ca8c
Angel Burgos Desktop <angelpublicista.1@gmail.com>
2021-04-17 00:41:12 -0500

    Componente cat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from "./components/Header/Header";
 import "./App.sass";
-
-
+import CardCat from "./components/CardCat/CardCat";
+import ImgBg from "./components/CardCat/img/image-cat.jpg";
 function App() {
   return (
     <div className="App">
-      <Header title="Start Up" />
+      <CardCat imgBg={ImgBg} text="Hola" style="style-2"/>
     </div>
   );
 }
---
+++ src/components/CardCat/CardCat.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import CardBg from "./img/image-cat.jpg";
+import ArrowCat from "./img/arrow-cat.svg";
+import "./CardCat.sass";
+import Grid from "@material-ui/core/Grid";
+
+class CardCat extends Component {
+  render(props) {
+    return (
+      <a href="#" className={this.props.style + " du-card-cat"}>
+        <figure>
+          <img src={this.props.imgBg} className="du-card-cat__cover"></img>
+          <figcaption>
+            <h3 className="du-card-cat__title">{this.props.text}</h3>
+            <img src={ArrowCat}></img>
+          </figcaption>
+        </figure>
+      </a>
+    );
+  }
+}
+
+export default CardCat;
